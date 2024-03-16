2f1b4a7188efad07acf9ade117dfc16ab4c4e77f
jacobianoinverso <14jponcebep@gmail.com>
2024-03-15 19:39:57 -0600

    Conectar

--- Prácticas/Practica1/Practica1.py
+++ Prácticas/Practica1/Practica1.py
@@ -3,15 +3,42 @@ import pandas as pa
 import streamlit as st
 import scipy as spicy
 
-page_bg_img = '''
+original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
+st.markdown(original_title, unsafe_allow_html=True)
+
+
+# Set the background image
+background_image = """
 <style>
-body {
-background-image: url("https://cdn-p.smehost.net/sites/a8928da38df6414aae98564041b07ae0/wp-content/uploads/2016/09/Vicente-Fernandez-Header-1920x964.jpg");
-background-size: cover;
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
 }
 </style>
-'''
+"""
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(background_image, unsafe_allow_html=True)
+
+st.text_input("", placeholder="Streamlit CSS ")
+
+input_style = """
+<style>
+input[type="text"] {
+    background-color: transparent;
+    color: #a19eae;  // This changes the text color inside the input box
+}
+div[data-baseweb="base-input"] {
+    background-color: transparent !important;
+}
+[data-testid="stAppViewContainer"] {
+    background-color: transparent !important;
+}
+</style>
+"""
+st.markdown(input_style, unsafe_allow_html=True)
+
 
 st.title('Distribución de 100 tiros de monedas advanced')
+
