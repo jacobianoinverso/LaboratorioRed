b3d91545ee07c4d7cd4847575c809f9cfa06f295
jacobianoinverso <14jponcebep@gmail.com>
2024-03-16 19:00:15 -0600

    asdf

--- Prácticas/Practica1/Practica1.py
+++ Prácticas/Practica1/Practica1.py
@@ -45,25 +45,53 @@ st.title('Distribución de tiros de monedas')
 
 m = st.slider('Elija el número de tiros para graficar la distribución',1,100)
 
-data = pd.read_csv("fichas.csv")
-aa = data.loc[:m,'JC']
+def binom(x,n,p):
+    # print('binom(',x,n,p,')')
+    
+    x = int(x)
+    n = int(n)
+        
+    comb = math.comb(n,x)
+    p_x = p**x
+    q_nx = (1-p)**(n-x)
 
-plt.hist(x=range(1,10), bins=aa, color='#F2AB6D', rwidth=0.85)
-plt.title('Histograma de tiros de monedas')
-plt.xlabel('Cantidad de caras')
-plt.ylabel('Cantidad de veces')
-plt.xticks(range(1,10))
+    return comb*p_x*q_nx
+    # return A * scs.binom.pmf(x,n,p)
 
-st.pyplot()
+binom = np.vectorize(binom)
 
-# data.loc[:m,'JC']
+
+data = pd.read_csv(fichas.csv')
 print(f'data:\n{data}')
 
-fitted_results = ss.fit(ss.binom,data,bounds=[(0,100),(0,1)])
-print(fitted_results)
-fitted_results.plt()
-st.pyplot()
+data = data.loc[:m]
+
+counts_non_sort = data['GS'].value_counts()
+counts = pd.DataFrame(np.zeros(11))
+# print(counts)
+
+for row, value in counts_non_sort.items():
+    counts.loc[row,0] = value
+
+print(f'counts:\n{counts}')
+print(f'index: {counts.index.values}')
+print(f'normalized counts: {list(counts[0]/m)}')
+
+
+fit, cov_mat = sco.curve_fit(binom,counts.index.values,counts[0]/m,[10,0.5],bounds=[(0,0),(np.inf,1)])
+
+print(f'Fit:\n{fit}\ncov_mat\n{cov_mat}')
+
+n = fit[0]
+p = fit[1]
+
+print(f'Este es el valor de n: {n}\nEste es el valor de p: {p}')
 
 
 
 
+binomial_plot = px.line(x=counts.index.values, y=binom(counts.index.values,n,p), title="Lanzamiento de fichas")
+
+binomial_plot.add_bar(x=counts.index.values, y=counts[0]/m, name='Lanzamientos experimentales')
+
+binomial_plot.show()
