ff34a6ec13b0775f668dbbc4353a9c81a52e7f1f
jacobianoinverso <14jponcebep@gmail.com>
2024-03-16 20:34:12 -0600

    hgfd

--- Prácticas/Practica1/Practica1.py
+++ Prácticas/Practica1/Practica1.py
@@ -48,7 +48,7 @@ st.title('Distribución de tiros de monedas')
 
 st.write('Con ayuda de este programa, usted podrá ver los datos de 100 tiros de 10 monedas ajustarse a una distribución binomial.')
 
-st.markdown("[Section 1](#distribución-de-tirs-de-monedas)")
+st.markdown("[Section 1](#distribución-de-tiros-de-monedas)")
 
 m = st.slider('Elija el número de tiros para graficar la distribución',1,100)
 
