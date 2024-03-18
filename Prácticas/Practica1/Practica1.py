4a5c6c92c8b3552365f790cf7d13d6e45eb1699a
jacobianoinverso <14jponcebep@gmail.com>
2024-03-17 20:04:11 -0600

    asdf

--- Prácticas/Practica1/Practica1.py
+++ Prácticas/Practica1/Practica1.py
@@ -279,7 +279,7 @@ st.text('Tenemos dos formas de calcular incertidumbres:')
 st.text('Cuando tenemos un grupo de datos medidos independentes, usamos la desviación estandar:')
 st.latex(r'''\sigma = \sqrt{\frac{1}{N}\sum^N_{i=1}(x_i-\bar x)^2}''')
 st.text('Cuando tenemos variables dependientes y queremos sacar su incertidumbre memorizando cada fórmula\no podemos resolverlo como adultos de la forma:')
-st.latex(r'''\sigma_f=\sqrt{\sum^N_{i=1}\left(\frac{\partialf}{\partialx_i}\right)^2\sigma_{x_i}^2}''')
+st.latex(r'''\sigma_f=\sqrt{\sum^N_{i=1}\left(\frac{\partial f}{\partial x_i}\right)^2\sigma_{x_i}^2}''')
 
 
 
