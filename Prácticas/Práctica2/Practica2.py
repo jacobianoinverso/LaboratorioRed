08e1a4e7b34fb890855238547674a01e6a32ef6e
jacobianoinverso <14jponcebep@gmail.com>
2024-04-09 19:58:07 -0600

    ασδφασδ

--- Prácticas/Práctica2/Practica2.py
+++ Prácticas/Práctica2/Practica2.py
@@ -8,34 +8,5 @@ import plotly.express as px
 from scipy import stats as ss
 
 
-background_image = """
-<style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://cloudfront-us-east-1.images.arcpublishing.com/gruponacion/LEBXCHHCNJFMNBOYLMZJ4RJYQM.jpg");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
-    background-repeat: no-repeat;
-}
-</style>
-"""
-
-st.markdown(background_image, unsafe_allow_html=True)
-
-
-input_style = """
-<style>
-input[type="text"] {
-    background-color: transparent;
-    color: #a19eae;  // This changes the text color inside the input box
-}
-div[data-baseweb="base-input"] {
-    background-color: transparent !important;
-}
-[data-testid="stAppViewContainer"] {
-    background-color: transparent !important;
-}
-</style>
-"""
-
 
 st.markdown("Θέλω μία πόρνη")
