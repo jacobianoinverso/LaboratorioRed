cdcd0577055291c5dc99b1c91be0fe2b97204278
jacobianoinverso <14jponcebep@gmail.com>
2024-04-22 04:25:32 -0600

    asdf

--- Tareas/Examenparcial1/Parcial.py
+++ Tareas/Examenparcial1/Parcial.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt    #importamos los paquetes instalados en el ent
 import numpy as np
 import seaborn as sns
 import streamlit as st
-
+from scipy import stats as sts
 st.title('Graficadora de distribuciones binomiales')
-st.write('He aquí una erramienta que permite representar de forma gráfica la cantidad de veces que un suceso tiene éxito.')
+st.write('He aquí una herramienta que permite representar de forma gráfica la probabilidad de que un suceso tenga éxito en repetidas ocasiones.')
 
 
 #Pedimos al usuario que inserte los valores de la distribución binomial
@@ -13,7 +13,10 @@ n = st.number_input('Inserte el número de tiros n\n', min_value=1, max_value=10
 
 p = st.number_input('Inserte la posibilidad de éxito p\n', min_value=0.01, max_value=1.00, step=0.01, value=0.50, help='insertar un valor entre 0 y 1')
 
-q=1-p 
-
-DistBi = np.random.binomial(n=n,p=p,size=10)
-st.write(DistBi)
+x = np.arange(0, n+1)
+pmf = binom.pmf(x, n, p)  
+plt.bar(x, pmf)  
+plt.xlabel('Number of Heads')  
+plt.ylabel('Probability')  
+plt.title('Binomial Distribution - Coin Flips')  
+plt.show()  
