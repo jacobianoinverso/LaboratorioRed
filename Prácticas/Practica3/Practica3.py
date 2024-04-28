bb664f18a9284d3ca6edb8d5295064b9c881abe4
jacobianoinverso <14jponcebep@gmail.com>
2024-04-27 21:47:36 -0600

    asdf

--- Prácticas/Practica3/Practica3.py
+++ Prácticas/Practica3/Practica3.py
@@ -23,6 +23,11 @@ st.write("El isótopo que estudiaremos hoy, el cesio-137, recibe su nombre del h
 st.header("Cesio-137")
 st.write("Es uno de los isótopos más estables de cesio. Se obtiene de forma sintética a partir de fisión nuclear. El Cesio-137 es un emisor beta, el 94,6 por ciento se desintegra en Ba-137m y el resto directamente en Ba-137 que es estable. La energía de los fotones de Ba-137m es 661,7 keV. Estos fotones pueden ser utilizados en la irradiación de los alimentos, o de la radioterapia en el cáncer. Cs-137 es menos utilizado para la radiografía industrial que otros isótopos gamma. Se puede encontrar en algunos medidores de humedad y de densidad, medidores de flujo, sensores y otros equipos.")
 
+st.header("Decaimiento radiactivo")
+st.write("Es la reducción del núcleo de un isótopo inestable en una muestra de materia en un periodo de tiempo que varía de isótopo a isótopo. Toma lugar hasta que toda la sustancia se convierte en algún elemento más estable")
+
 st.header("Distribución Gaussiana")
 st.write("También llamada distribución normal, es una distribución probabilística de variable continua simétrica con respecto a un parámetro. Por ls forma acampanada de su función de densidad, se le suele llamar campana de Gauss. Es ampliamente usada porque permite modelar diversos fenómenos incluso de ramas de estudio diferentes.")
 st.header("Fórmulas relevantes")
+st.write("Densidad de probabilidad")
+st.latex(r'''pdf = \frac{1}{\sigma\sqrt{2\pi}}''')
