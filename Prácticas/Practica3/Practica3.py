b4192c31a529a02134f5cd2c57b34a1e810e1377
jacobianoinverso <14jponcebep@gmail.com>
2024-04-27 20:05:44 -0600

    sdfg

---
+++ Prácticas/Practica3/Practica3.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy import optimize as sco
+import math
+import plotly.express as px
+from scipy import stats as ssτ
+from scipy import special as ssp
+
+st.title('Decaimiento radioactivo del Cesio-137')
