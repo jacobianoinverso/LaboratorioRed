ea836247a8f45b683281661789b7bf77988fcf94
jacobianoinverso <14jponcebep@gmail.com>
2024-04-27 20:19:52 -0600

    sdfg

--- Prácticas/Practica3/Practica3.py
+++ Prácticas/Practica3/Practica3.py
@@ -9,3 +9,7 @@ from scipy import stats as ssτ
 from scipy import special as ssp
 
 st.title('Decaimiento radioactivo del Cesio-137')
+st.write("Jacobo Ponce")
+st.write("César García")
+
+st.write("En este experimento, vamos a analizar la cantidad de partículas en el aire y las emitidas por una muestra de cesio-137 en decadencia y vamos a ajustar los resultados utilizando las distribuciones de Gauss y de Poisson. Para saber si estos ajustes son correctos, haremos la prueba de χ² para cada uno de los ajustes.")
